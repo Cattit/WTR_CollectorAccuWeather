37cb0881894b6cc1e31ead8440704bef66a28594
Cattit <ekaterina.titaeva@gmail.com>
2019-03-01 12:40:23 +0800

    WT-4; get forecast data

--- data/AccuWeather.js
+++ data/AccuWeather.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 let dal = require("wtr-dal");
 let source = "AccuWeather"
 const dateNow = new Date()
-dateNow.setHours(dateNow.getHours(), 0, 0, 000)
+dateNow.setHours(0, 0, 0, 000)
 
 function dateDay(amount_day) {
     return {
@@ -22,7 +22,7 @@ function dateNight(amount_day) {
     }
 }
 
-function rainfall(forecast_snow, forecast_rain, forecast_grad, forecast_storm) {  // не возвращает инфу о яп, только о дожде, снеге, граде и снеге с дождем
+function rainfall(forecast_snow, forecast_rain, forecast_grad, forecast_storm, wind, temperature, amount_rainfall) {  // не возвращает инфу о яп, только о дожде, снеге, граде и снеге с дождем
     return {
         snow: (forecast_snow !== 0 && forecast_rain === 0) ? 1 : 0,
         rain: (forecast_rain !== 0 && forecast_snow === 0) ? 1 : 0,
@@ -32,28 +32,45 @@ function rainfall(forecast_snow, forecast_rain, forecast_grad, forecast_storm) {
         storm: forecast_storm !== 0 ? 1 : 0,
         drizzle: null,
         rainsnow: (forecast_rain !== 0 && forecast_snow !== 0) ? 1 : 0,
-        grad: forecast_grad !== 0 ? 1 : 0
+        grad: forecast_grad !== 0 ? 1 : 0,
+        hard_wind: wind >= 15 ? 1 : 0,
+        hard_heat: (dateNow.getMonth() >= 3 && dateNow.getMonth() <= 8 && temperature >= 40) ? 1 : 0,
+        hard_frost: ((dateNow.getMonth() >= 9 || dateNow.getMonth() <= 2) && temperature <= -35) ? 1 : 0,
+        hard_rainfall: (((forecast_snow !== 0 || forecast_grad !== 0) && forecast_rain === 0 && amount_rainfall >= 7) || amount_rainfall >= 15) ? 1 : 0 // если снега не менее 7 мм или осадков не менее 15 мм
     }
 }
 
 async function analyzeData(code_location, { DailyForecasts }) {
     let dataAll = [];
     for (let dd = 1; dd < 4; dd += 2) {
+
+        let temperature = DailyForecasts[dd].Temperature.Maximum.Value,
+            wind_speed_from = DailyForecasts[dd].Day.Wind.Speed.Value * 10 / 36,    //перевод в м/с
+            wind_speed_to = DailyForecasts[dd].Day.Wind.Speed.Value * 10 / 36,
+            wind_gust = DailyForecasts[dd].Day.WindGust.Speed.Value * 10 / 36,
+            amount_rainfall = DailyForecasts[dd].Day.Rain.Value + DailyForecasts[dd].Day.Ice.Value + 10 * DailyForecasts[dd].Day.Snow.Value // переводим снег в мм
+
         dataAll.push(
             {
                 code_location: code_location,
                 source,
                 depth_forecast: dd,
                 date: dateDay(dd),
-                temperature: DailyForecasts[dd].Temperature.Maximum.Value,
+                temperature: temperature,
                 wind_speed: {
-                    from: DailyForecasts[dd].Day.Wind.Speed.Value * 10 / 36,    //перевод в м/с
-                    to: DailyForecasts[dd].Day.Wind.Speed.Value * 10 / 36
+                    from: wind_speed_from,
+                    to: wind_speed_to
                 },
-                wind_gust: DailyForecasts[dd].Day.WindGust.Speed.Value * 10 / 36,
-                rainfall: rainfall(DailyForecasts[dd].Day.Snow.Value, DailyForecasts[dd].Day.Rain.Value, DailyForecasts[dd].Day.Ice.Value, DailyForecasts[dd].Day.ThunderstormProbability),
-                amount_rainfall: DailyForecasts[dd].Day.Rain.Value + DailyForecasts[dd].Day.Ice.Value + 10 * DailyForecasts[dd].Day.Snow.Value    // переводим снег в мм
+                wind_gust: wind_gust,
+                amount_rainfall: amount_rainfall,
+                rainfall: rainfall(DailyForecasts[dd].Day.Snow.Value, DailyForecasts[dd].Day.Rain.Value, DailyForecasts[dd].Day.Ice.Value, DailyForecasts[dd].Day.ThunderstormProbability, Math.max(wind_speed_from, wind_speed_to, wind_gust), temperature, amount_rainfall),
             })
+
+        temperature = DailyForecasts[dd].Temperature.Minimum.Value
+        wind_speed_from = DailyForecasts[dd].Night.Wind.Speed.Value * 10 / 36
+        wind_speed_to = DailyForecasts[dd].Night.Wind.Speed.Value * 10 / 36
+        wind_gust = DailyForecasts[dd].Night.WindGust.Speed.Value * 10 / 36
+        amount_rainfall = DailyForecasts[dd].Night.Rain.Value + DailyForecasts[dd].Night.Ice.Value + 10 * DailyForecasts[dd].Night.Snow.Value
 
         dataAll.push(
             {
@@ -61,14 +78,14 @@ async function analyzeData(code_location, { DailyForecasts }) {
                 source,
                 depth_forecast: dd,
                 date: dateNight(dd),
-                temperature: DailyForecasts[dd].Temperature.Minimum.Value,
+                temperature: temperature,
                 wind_speed: {
-                    from: DailyForecasts[dd].Night.Wind.Speed.Value * 10 / 36,
-                    to: DailyForecasts[dd].Night.Wind.Speed.Value * 10 / 36
+                    from: wind_speed_from,
+                    to: wind_speed_to
                 },
-                wind_gust: DailyForecasts[dd].Night.WindGust.Speed.Value * 10 / 36,
-                rainfall: rainfall(DailyForecasts[dd].Night.Snow.Value, DailyForecasts[dd].Night.Rain.Value, DailyForecasts[dd].Night.Ice.Value, DailyForecasts[dd].Night.ThunderstormProbability),
-                amount_rainfall: DailyForecasts[dd].Night.Rain.Value + DailyForecasts[dd].Night.Ice.Value + 10 * DailyForecasts[dd].Night.Snow.Value
+                wind_gust: wind_gust,
+                amount_rainfall: amount_rainfall,
+                rainfall: rainfall(DailyForecasts[dd].Night.Snow.Value, DailyForecasts[dd].Night.Rain.Value, DailyForecasts[dd].Night.Ice.Value, DailyForecasts[dd].Night.ThunderstormProbability, Math.max(wind_speed_from, wind_speed_to, wind_gust), temperature, amount_rainfall),
             })
 
     }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let dal = require("wtr-dal");
 async function startDataСollection() {
   let code_location = 2515263
   const dataForecast = await getData.getforecast(code_location);
+  // console.log(dataForecast)
   const id_forecast = await dal.saveForecast(dataForecast[0].source, dataForecast[0].date.now);
   const id_location = await dal.getIdLocationByCode(code_location)
   await dataForecast.map(forecast => dal.saveForecastData(forecast, id_forecast, id_location));
